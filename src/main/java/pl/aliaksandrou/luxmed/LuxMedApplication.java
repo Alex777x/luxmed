bee1efd4114dcf29afb59f11b4b516408b046cd7
Anatoli Aliaksandrou <alexandroff103@gmail.com>
2024-10-07 17:22:06 +0200

    Initial commit

---
+++ src/main/java/pl/aliaksandrou/luxmed/LuxMedApplication.java
@@ -0,0 +1,13 @@
+package pl.aliaksandrou.luxmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxMedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxMedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/aliaksandrou/luxmed/LuxMedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aliaksandrou.luxmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxMedApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
